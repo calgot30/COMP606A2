5bdb425e8ef23aed39a0f4792664e86362d2b74d
Callum <callumgott@gmail.com>
2019-10-22 14:03:26 +1300

    added files

---
+++ logout.php
@@ -0,0 +1,14 @@
+<!-- 
+    Logout feature which is available on the home page, destroys the 
+    session and sends the user back to the home page.
+    
+ -->
+
+ <?php
+session_start();
+
+if(session_destroy()){
+    header("Location: Home.php");
+}
+
+?>
