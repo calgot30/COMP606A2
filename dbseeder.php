8450cef83dbe4e8d770a5bf253d097e8d310f259
Callum <callumgott@gmail.com>
2019-10-29 15:44:06 +1300

    what I've attempted with the seeder so far, no luck dropping db and then creating and inserting info

---
+++ dbseeder.php
@@ -0,0 +1,61 @@
+
+<?php
+
+$_host = "localhost";
+$_username = "guest";
+$_password = "";
+$_database = "safetrade";
+
+
+$mysqli = mysqli_connect($_host, $_username, $_password, $_database);
+if ($mysqli == false){
+    header("location: sitedown.php");
+  }
+
+// make sure we see all errors
+error_reporting(E_ALL);
+
+
+echo "<h3>*** DATABASE SEEDER RUNNING ***</h3>";
+echo "creating table products ...<br>";
+$mysqli->query("DROP TABLE IF EXISTS tradesman");
+
+
+$sql = <<<EOD
+CREATE TABLE `tradesman` (
+    `tradesman_id` int(11) NOT NULL,
+    `tradesman_name` varchar(255) NOT NULL,
+    `tradesman_email` varchar(255) NOT NULL,
+    `tradesman_password` varchar(255) NOT NULL,
+    `tradesman_number` int(20) NOT NULL,
+    `area` varchar(30) NOT NULL,
+    `trade_name` varchar(30) NOT NULL
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+EOD;
+
+$mysqli->query($sql);
+echo "populating table products ...<br>";
+
+$sql = <<<EOD
+
+
+INSERT INTO `tradesman` (`tradesman_id`, `tradesman_name`, `tradesman_email`, `tradesman_password`, `tradesman_number`, `area`, `trade_name`) VALUES 
+(1, 'bob the builder', 'bob@builder.com', 'passwordbob', 3214896, 'Northland', 'Building & carpentry'),
+('Sophey Thornber', 'sthornber1@wiley.com', 'o6qlSczJgS7', '1498121792', null, null),
+('Fidelio Jeayes', 'fjeayes2@wikispaces.com', 'FbpEBWB5fq', '7091660601', null, null),
+('Cooper Anfossi', 'canfossi3@craigslist.org', 'mH3nd30b35eY', '6982482977', null, null),
+('Silas Pennoni', 'spennoni4@wp.com', 'JomEOxEiG', '8043863931', null, null),
+('Monah Freshwater', 'mfreshwater5@ucoz.com', 'PieEWvCFk', '6246966939', null, null),
+('Angel Bartleet', 'abartleet6@addthis.com', '9lpIE3CZh89', '1786527441', null, null),
+('Meggi Frankis', 'mfrankis7@bbb.org', 'gShSC3W2jS', '2876425175', null, null),
+('Noam Veneur', 'nveneur8@amazon.co.jp', 's64uePYDUw8', '8363264870', null, null);
+
+EOD;
+$mysqli->multi_query($sql);
+echo "retrieving tables in $database ...<br>";
+$result = $mysqli->query("SHOW TABLES");
+while($row = $result->fetch_row()){
+  echo $row[0];
+}
+echo "<h3>*** DATABASE SEEDER FINISHED ***</h3>";
+?>
