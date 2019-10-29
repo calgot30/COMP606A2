563641928074c1df63d11645ba3a93d61531ea28
Callum <callumgott@gmail.com>
2019-10-29 15:51:01 +1300

    didn't save

---
+++ dbconnect.php
@@ -0,0 +1,15 @@
+  
+<?php
+error_reporting(0);
+$user = "guest";
+$password = "";
+$database = "safetrade";
+$host = "localhost";
+$mysqli = mysqli_connect($host, $user, $password, $database);
+if ($mysqli == false){
+  header("location: sitedown.php");
+}
+$mysqli->autocommit(true);
+// make sure we see all errors
+error_reporting(E_ALL);
+?>
