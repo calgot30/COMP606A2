c0d0ffcff1470519abd2b71d3f2c0d78845fe97f
siesaurus <52937221+siesaurus@users.noreply.github.com>
2019-11-04 20:05:25 +1300

    added contact us page

---
+++ contactus.php
@@ -0,0 +1,24 @@
+<?php include("header.php");
+
+// if(isset($_SESSION["loggedin"]) == false){
+//     header("Location: titlepage.php");
+// }
+?>
+
+<html>
+<head>
+</head>
+<body>
+<div class="row">
+<div class="col-6">
+<h2> Reach out to us today</h2>
+<div class="col-8">
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</div>
+</div>
+<!-- https://www.publicdomainpictures.net/en/view-image.php?image=241603&picture=builder -->
+<div class="col-6">
+<img src="Assets/builder.jpg" alt="builder" height="500" width="350">
+</div>
+</div>
+</body>
+</html>
